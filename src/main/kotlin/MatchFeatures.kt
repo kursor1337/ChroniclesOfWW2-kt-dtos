8ec96f213b95c834deec78249e9120f28c4cc41d
kursor <skurochkin298@gmail.com>
2022-10-10 01:48:35 +0300

    added matching features

---
+++ src/main/kotlin/MatchFeatures.kt
@@ -0,0 +1,13 @@
+package com.kursor.chroniclesofww2.features
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class MatchingGameMessageDTO(
+    val type: MatchingGameMessageType,
+    val message: String
+)
+
+enum class MatchingGameMessageType {
+    MESSAGE, GAME_DATA, TIMEOUT, ACCEPT, REJECT
+}
